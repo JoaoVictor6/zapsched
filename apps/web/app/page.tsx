338a8e57ffd6b6f3c9cdac9bfdd97d1dc52fa0f2
João Victor <santosjoao.dev@gmail.com>
2024-09-20 12:31:51 -0300

    feat: initiate copy of landing page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,33 @@
 import { twMerge } from "tailwind-merge";
 
-export default function Home() {
-  const defaultCircleStyle = 'w-[200px] h-[200px] z-1 rounded-full absolute blur-[167px]';
+const BackgroundCircles = () => {
+  const defaultCircleStyle = 'w-[300px] h-[300px] z-1 rounded-full absolute blur-[267px]';
+
   return (
-      <main className="w-screen h-screen bg-[#000914] relative">
-        <div className={twMerge(defaultCircleStyle, 'bg-[#3A86FF] bottom-4 right-4')} />
-        <div className={twMerge(defaultCircleStyle, 'bg-[#7CB518] top-4 left-4')} />
-      </main>
+    <>
+      <div className={twMerge(defaultCircleStyle, 'bg-[#3A86FF] bottom-4 right-80')} />
+      <div className={twMerge(defaultCircleStyle, 'bg-[#7CB518] top-7 left-20')} />
+    </>
+  )
+}
+
+const HomeCopy = () => {
+  return (
+    <section className="p-4 grid gap-6">
+      <h1 className="font-bold text-[40px] text-white">
+        Facilite sua vida,<br /> organize sua agenda
+      </h1>
+      <p className="font-semibold text-[20px] text-white max-w-[25vw] leading-relaxed">
+        Automatize sua agenda no WhatsApp sem esforço. O zapSched organiza, notifica e gerencia tudo para você. Tenha mais tempo para o que importa!
+      </p>
+    </section>
+  );
+}
+export default function Home() {
+  return (
+    <main className={twMerge('w-screen h-screen bg-[#000914] relative', 'flex justify-center items-center')}>
+      <BackgroundCircles />
+      <HomeCopy />
+    </main>
   );
 }
